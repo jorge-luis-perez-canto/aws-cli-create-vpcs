a6d4f31d078a0fc38dcb37f4182ec72a58b25048
Jorge Luis Pérez Canto <70923213+jorge-luis-perez-canto@users.noreply.github.com>
2024-10-05 17:49:52 -0600

    Update aws-cli-check-resources.sh

--- aws-cli-check-resources.sh
+++ aws-cli-check-resources.sh
@@ -34,7 +34,7 @@ function check_resource() {
     else
         echo -e "${RED}No hay ID registrado para ${RESOURCE_TYPE}.${NC}"
     fi
-    echo -e "\n"  # Añade una línea en blanco para separar las verificaciones
+    echo -e ""  # Añade una línea en blanco para separar las verificaciones
 }
 
 # Leer y verificar cada recurso del archivo
