3048f70f75b74185e99d7ddd25c6c1fc6aa2168f
Jorge Luis Pérez Canto <70923213+jorge-luis-perez-canto@users.noreply.github.com>
2024-10-05 19:34:22 -0600

    Update aws-cli-check-resources.sh

--- aws-cli-check-resources.sh
+++ aws-cli-check-resources.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #******************************************************************************
+#    Script de Shell para verificar la existencia de recursos de AWS y resumir
+#******************************************************************************
 
 AWS_REGION="us-east-1"
 RESOURCE_FILE="aws_resources.txt"
@@ -10,6 +12,10 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # Sin color
 
+# Arrays para almacenar el estado de los recursos
+still_exists=()
+not_exists=()
+
 # Función para verificar cada recurso
 function check_resource() {
     local RESOURCE_TYPE=$1
@@ -18,22 +24,26 @@ function check_resource() {
 
     if [ "$RESOURCE_ID" != "None" ] && [ -n "$RESOURCE_ID" ]; then
         echo -e "${YELLOW}Verificando ${RESOURCE_TYPE} con ID ${RESOURCE_ID}...${NC}"
+        
+        # Ejecutar el comando con redirección de stderr a /dev/null
         eval $AWS_COMMAND 2>/dev/null
         local status=$?
+        
         if [ $status -eq 0 ]; then
             echo -e "${GREEN}${RESOURCE_TYPE} con ID ${RESOURCE_ID} todavía existe.${NC}"
+            still_exists+=("${RESOURCE_TYPE} ID ${RESOURCE_ID}")
         else
             echo -e "${RED}${RESOURCE_TYPE} con ID ${RESOURCE_ID} no existe o no se pudo verificar.${NC}"
+            not_exists+=("${RESOURCE_TYPE} ID ${RESOURCE_ID}")
         fi
     else
         echo -e "${RED}No hay ID registrado para ${RESOURCE_TYPE}.${NC}"
     fi
-    echo ""  # Añade solo una línea en blanco
+    echo ""
 }
 
 # Leer y verificar cada recurso del archivo
 while IFS=": " read -r key value; do
-    echo ""  # Asegura que solo hay un salto de línea antes de cada verificación
     case "$key" in
         VPC_ID) check_resource "VPC" "$value" "aws ec2 describe-vpcs --vpc-ids $value --region $AWS_REGION" ;;
         SUBNET_PUBLIC_ID) check_resource "Subred Pública" "$value" "aws ec2 describe-subnets --subnet-ids $value --region $AWS_REGION" ;;
@@ -47,3 +57,14 @@ while IFS=": " read -r key value; do
         MAIN_ROUTE_TABLE_ID) check_resource "Tabla de Rutas Principal" "$value" "aws ec2 describe-route-tables --route-table-ids $value --region $AWS_REGION" ;;
     esac
 done < "$RESOURCE_FILE"
+
+# Mostrar resumen al final
+echo -e "${GREEN}Recursos que aún existen:${NC}"
+for item in "${still_exists[@]}"; do
+    echo -e "${GREEN}$item${NC}"
+done
+
+echo -e "${RED}Recursos que no existen o no se pudieron verificar:${NC}"
+for item in "${not_exists[@]}"; do
+    echo -e "${RED}$item${NC}"
+done
